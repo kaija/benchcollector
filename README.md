67fa86324316b9608be5d436a0adc46b91820c0e
kaija <kaija.chang@gmail.com>
2016-01-04 04:20:50 +0000

    Add report page

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ server=racing.elasticservice.co
 
 wget http://example.com/scripts/benchmark_aws
 sh benchmark_aws example.com
+
+
+
+runcmd:
+ - wget http://example.com//scripts/benchmark_aws
+ - sh benchmark_aws example.com
+ - shutdown -h now
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,29 @@ var fx = require('mkdir-recursive');
 var targz = require('tar.gz');
 var multipart = require('connect-multiparty');
 var path = require('path');
+var async = require('async');
 var multipartMiddleware = multipart();
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/reports', function(req, res, next) {
+  var base = path.join(__dirname, '../public/report/');
+  fs.readdir(base, function(err, stat){
+    var items = {};
+    async.eachSeries(stat, function findInst(item, callback){
+      items[item] = [];
+      fs.readdir(base + item, function(err, stats){
+        items[item] = stats;
+        console.log(stats);
+        callback(null, items);
+      });
+    }, function done(err, result){
+      res.render('report_index', items);
+    });
+  });
 });
 
 router.post('/iozone', multipartMiddleware, function(req, res, next) {
